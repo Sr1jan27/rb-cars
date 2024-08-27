5f054fb5256fb2b1d0435156f905040c6f7133fc
srijan_singh <singhsrijan809@gmail.com>
2024-08-27 09:39:11 +0530

    services and contact components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ServicesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
